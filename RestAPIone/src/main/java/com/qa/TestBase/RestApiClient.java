2a00dbbcbe4f4d4c6cd2a1ca6d65592ad1f7618c
Madhu-Desktop <madhucvr1234@gmail.com>
2021-06-01 11:43:01 +0530

    imported Testng imports

--- RestAPIone/src/main/java/com/qa/TestBase/RestApiClient.java
+++ RestAPIone/src/main/java/com/qa/TestBase/RestApiClient.java
@@ -12,6 +12,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.testng.annotations.Test;
 
 public class RestApiClient {
 
